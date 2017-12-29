b2bc6fcdf44a31aa4c34204a3b93247672e9fd62
Jan Willems <jw@elevenbits.com>
2017-12-29 17:00:19 +0100

    Added optional logfile as argument.  Also added .gitignore.

--- tester.py
+++ tester.py
@@ -6,23 +6,24 @@ import sys
 import argparse
 
 
-logging.basicConfig(filename="speed-tester.log", level=logging.INFO)
-
-
 def abort(message, code=1):
     logging.error("Aborting: {0}".format(message))
     sys.exit(code)
 
 
-# get the host and the port
+# get the host and the port arguments
 description = "Tests the speed of your network connection and sends the result to a monitor."
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument("host", type=str,
                     help="The host where the monitor runs.")
 parser.add_argument("port", type=str,
                     help='The port where the monitor runs.')
+parser.add_argument("logfile", type=str, nargs="?", default="speed-tester.log",
+                    help="The log file.")
+
 args = parser.parse_args()
 
+logging.basicConfig(filename=args.logfile, level=logging.INFO)
 logging.info("Sending the results to monitor at {0}:{1}.".format(args.host, args.port))
 
 # the rester tester
@@ -30,17 +31,18 @@ s = speedtest.Speedtest()
 
 # first get client
 config = s.get_config()
-client = s.config['client']
+client = s.config["client"]
 
 # get or create it via rest
 try:
-    r = requests.get("http://localhost:8000/api/clients?ip={0}&isp={1}".format(client["ip"], client["isp"]))
+    r = requests.get("http://{0}:{1}/api/clients?ip={2}&isp={3}".
+                     format(args.host, args.port, client["ip"], client["isp"]))
     if r.ok and len(r.json()) == 1:
         logging.info("Found client {0} (id:{1}) ".format(r.json()[0]["isp"], r.json()[0]["id"]))
         client_id = r.json()[0]["id"]
     else:
         logging.info("Creating client {0} (ip:{1})".format(client["isp"], client["ip"]))
-        r = requests.post('http://localhost:8000/api/clients', json=s.config['client'])
+        r = requests.post("http://{0}:{1}/api/clients".format(args.host, args.port), json=s.config['client'])
         if r.ok:
             logging.info("Created client {0} (id:{1}) ".format(r.json()["isp"], r.json()["id"]))
             client_id = r.json()["id"]
@@ -54,13 +56,13 @@ s.get_best_server()
 
 # get or create it via rest
 try:
-    r = requests.get('http://localhost:8000/api/servers?url={0}'.format(s.best["url"]))
+    r = requests.get('http://{0}:{1}/api/servers?url={0}'.format(args.host, args.port, s.best["url"]))
     if r.ok and len(r.json()) == 1:
         logging.info("Found server {0} (id:{1}) ".format(r.json()[0]["url"], r.json()[0]["id"]))
         server_id = r.json()[0]["id"]
     else:
         logging.info("Creating server {0} (name:{1})".format(s.best["url"], s.best["name"]))
-        r = requests.post('http://localhost:8000/api/servers', json=s.best)
+        r = requests.post("http://{0}:{1}/api/servers".format(args.host, args.port), json=s.best)
         if r.ok:
             logging.info("Created server {0} (id:{1}) ".format(r.json()["url"], r.json()["id"]))
             server_id = r.json()["id"]
@@ -81,7 +83,7 @@ result["client"] = client_id
 result["server"] = server_id
 
 # ...and send it to the monitor
-r = requests.post('http://localhost:8000/api/results', json=result)
+r = requests.post("http://{0}:{1}/api/results".format(args.host, args.port), json=result)
 if r.ok:
     logging.info("Added result [up:{0}, down:{1}] ".format(result["upload"], result["download"]))
 else:
