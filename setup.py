77126893c04e9c151f40906705f2666762de0c5e
Jan Willems <jw@elevenbits.com>
2017-12-29 19:43:16 +0100

    Created basic setup.py.

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
+
+setup(
+    name="speed-tester",
+    version="0.1.0",
+    description="Test the speed of your network connection and "
+                "send the result to a monitor.",
+    url="https://github.com/jw/speed-tester",
+    author='Jan Willems',
+    author_email="jw@elevenbits.com",
+    license="MIT",
+    py_modules=['tester', 'scheduler'],
+    install_requires=requirements,
+    python_requires='~=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: Dutch',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: System :: Networking',
+    ],
+)
