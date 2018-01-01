1aa73fbf0a4c98ec9edce67efe49fe1ad9b2a65a
Jan Willems <jw@elevenbits.com>
2018-01-01 12:37:27 +0100

    Added --console and --verbose options.

--- speedtester.py
+++ speedtester.py
@@ -8,24 +8,47 @@ import sys
 import argparse
 from datetime import datetime
 
+logger = logging.getLogger('speedlogger')
+
 
 def abort(message, code=1):
-    logging.error("Aborting: {0}".format(message))
+    logger.error("Aborting: {0}".format(message))
     sys.exit(code)
 
 
 def main():
     args = get_parser()
-    logging.basicConfig(filename=args.logfile, level=logging.INFO)
+    create_logger(args)
     perfrom_test(args.host, args.port)
+
+
+def create_logger(args):
+    """
+    Create a logger.  If --console is given, also log to console; if
+    --verbse is set, enable the Logging.DEBUG.
+    :param args:
+    :return:
+    """
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    fh = logging.FileHandler(args.logfile)
+    logger.addHandler(fh)
+
+    if args.console:
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        logger.addHandler(ch)
 
 
 def perfrom_test(host, port):
     now = datetime.now()
 
-    logging.info("Starting the test at {0}.".format(now))
-    logging.info("Sending the results to monitor at {0}:{1}.".
-                 format(host, port))
+    logger.info("Starting the test at {0}.".format(now))
+    logger.info("Sending the results to monitor at {0}:{1}.".
+                format(host, port))
 
     # the real tester
     s = speedtest.Speedtest()
@@ -38,17 +61,18 @@ def perfrom_test(host, port):
         r = requests.get("http://{0}:{1}/api/clients?ip={2}&isp={3}".
                          format(host, port, client["ip"], client["isp"]))
         if r.ok and len(r.json()) == 1:
-            logging.info("Found client {0} (id:{1}).".
-                         format(r.json()[0]["isp"], r.json()[0]["id"]))
+            logger.info("Found client {0} (id:{1}).".
+                        format(r.json()[0]["isp"], r.json()[0]["id"]))
             client_id = r.json()[0]["id"]
         else:
-            logging.info("Creating client {0} (ip:{1})...".
-                         format(client["isp"], client["ip"]))
+            logger.info("Creating client {0} (ip:{1})...".
+                        format(client["isp"], client["ip"]))
+            logger.debug("Creating client using {0}.".format(client))
             r = requests.post("http://{0}:{1}/api/clients".
                               format(host, port), json=client)
             if r.ok:
-                logging.info("Created client {0} (id:{1}).".
-                             format(r.json()["isp"], r.json()["id"]))
+                logger.info("Created client {0} (id:{1}).".
+                            format(r.json()["isp"], r.json()["id"]))
                 client_id = r.json()["id"]
             else:
                 abort("Could not get or create the client!")
@@ -60,20 +84,23 @@ def perfrom_test(host, port):
 
     # get or create it via rest
     try:
-        r = requests.get('http://{0}:{1}/api/servers?url={0}'.
-                         format(host, port, s.best["url"]))
+        r = requests.get('http://{0}:{1}/api/servers?host={0}'.
+                         format(host, port, s.best["host"]))
         if r.ok and len(r.json()) == 1:
-            logging.info("Found server {0} (id:{1}).".
-                         format(r.json()[0]["url"], r.json()[0]["id"]))
+            logger.info("Found server {0} (id:{1}).".
+                        format(r.json()[0]["host"], r.json()[0]["id"]))
             server_id = r.json()[0]["id"]
         else:
-            logging.info("Creating server {0} (name:{1})...".
-                         format(s.best["url"], s.best["name"]))
+            logger.info("Creating server {0} (name:{1})...".
+                        format(s.best["host"], s.best["name"]))
+            s.best['identifier'] = s.best['id']
+            del s.best['id']
+            logger.debug("Creating server using {0}.".format(s.best))
             r = requests.post("http://{0}:{1}/api/servers".
                               format(host, port), json=s.best)
             if r.ok:
-                logging.info("Created server {0} (id:{1}).".
-                             format(r.json()["url"], r.json()["id"]))
+                logger.info("Created server {0} (id:{1}).".
+                            format(r.json()["host"], r.json()["id"]))
                 server_id = r.json()["id"]
             else:
                 abort("Could not get or create the server!")
@@ -81,9 +108,9 @@ def perfrom_test(host, port):
         abort("Could not connect to the monitor; is it running?")
 
     # test the speed of the link
-    logging.info("Getting download speed...")
+    logger.info("Getting download speed...")
     s.download()
-    logging.info("Getting upload speed...")
+    logger.info("Getting upload speed...")
     s.upload()
 
     # prepare the result...
@@ -93,11 +120,12 @@ def perfrom_test(host, port):
 
     # ...and send it to the monitor
     try:
+        logger.debug("Sending this result: {0}.".format(result))
         r = requests.post("http://{0}:{1}/api/results".
                           format(host, port), json=result)
         if r.ok:
-            logging.info("Sent result [up:{0}, down:{1}].".
-                         format(result["upload"], result["download"]))
+            logger.info("Sent result [up:{0}, down:{1}].".
+                        format(result["upload"], result["download"]))
         else:
             abort("Could not send the result to the monitor!")
     except ConnectionError:
@@ -105,8 +133,8 @@ def perfrom_test(host, port):
 
     later = datetime.now()
 
-    logging.info("Stopping the test at {0}; took {1}.".format(later,
-                                                              later - now))
+    logger.info("Stopping the test at {0}; took {1}.".format(later,
+                                                             later - now))
 
 
 def get_parser():
@@ -120,6 +148,10 @@ def get_parser():
                         help='The port where the monitor runs.')
     parser.add_argument("logfile", type=str, nargs="?",
                         default="speed-tester.log", help="The log file.")
+    parser.add_argument("--console", action="store_true",
+                        help="Also log to console.")
+    parser.add_argument("--verbose", action="store_true",
+                        help="Log more verbosely (i.e. in DEBUG mode).")
     args = parser.parse_args()
     return args
 
--- test_tester.py
+++ test_tester.py
@@ -1,6 +1,2 @@
-import speedtester
-import pytest
-
-
 def test_noop():
     assert True
